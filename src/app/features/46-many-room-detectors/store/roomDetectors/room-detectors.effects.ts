377412fb6b7d3bd78a8a58898a67bf0987f27286
Piotr Woloszun <piotr.woloszun@gmail.com>
2021-05-26 07:15:59 +0200

    room detectors task

--- src/app/features/46-many-room-detectors/store/roomDetectors/room-detectors.effects.ts
+++ src/app/features/46-many-room-detectors/store/roomDetectors/room-detectors.effects.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { RoomDetector, RoomTemperatureApiService } from '@api/room-temperature-api.service';
-import { Actions, Effect, ofType } from '@ngrx/effects';
+import { Actions, createEffect, ofType } from '@ngrx/effects';
 import { Action, select, Store } from '@ngrx/store';
 import { concat, interval, Observable, of, race } from 'rxjs';
 import { filter, map, mergeMap, takeUntil, withLatestFrom } from 'rxjs/operators';
@@ -12,43 +12,11 @@ import * as selectors from './room-detectors.selectors';
 const SYNC_FRAME_IN_MS = 2600;
 
 function stopSyncingRoom$(id: number, action$: Observable<Action>) {
-  const stopAll$ = action$.pipe(
-    ofType(actions.RoomDetectorsActionTypes.StopAllRoomsTracking)
-  );
-
-  const stopByRoomId$ = action$.pipe(
-    ofType(actions.RoomDetectorsActionTypes.StopSingleRoomTracking),
-    filter((action) => {
-      const actualId = (action as any).id;
-      return actualId === id;
-    })
-  );
-
-  return race(
-    stopAll$,
-    stopByRoomId$,
-  );
+  // TODO
 }
 
 function fetchRoomTemperature$(roomTemperatureApiService: RoomTemperatureApiService, id: number) {
-  const preRequest$ = of(actions.loadSingleRoomDetectorRequest({ id }));
-
-  const request$ = roomTemperatureApiService
-    .temperatureFor(id)
-    .pipe(
-      map((roomDetector: RoomDetector) => {
-        const roomDetectorUpdate = {
-          id: roomDetector.id,
-          changes: roomDetector,
-        };
-        return actions.loadSingleRoomDetectorSuccess({ roomDetectorUpdate });
-      }),
-    );
-
-  return concat(
-    preRequest$,
-    request$
-  );
+  // TODO
 }
 
 function syncRoomTemperatureTask$(
@@ -56,18 +24,7 @@ function syncRoomTemperatureTask$(
   id: number,
   action$: Observable<Action>
 ) {
-  const syncingStarted$ = of(actions.startSingleRoomTrackingSuccess({ id }));
-  const syncingTask$ = interval(SYNC_FRAME_IN_MS).pipe(
-    takeUntil(stopSyncingRoom$(id, action$)),
-    mergeMap(() => {
-      return fetchRoomTemperature$(roomTemperatureApiService, id);
-    }),
-  );
-
-  return concat(
-    syncingStarted$,
-    syncingTask$
-  );
+  //TODO
 }
 
 @Injectable()
@@ -76,34 +33,9 @@ export class RoomDetectorsEffects {
     select(selectors.selectRoomDetectorsIsRunning),
   );
 
-  @Effect()
-  loadManyRoomDetectors$ = this.actions$.pipe(
-    ofType(actions.RoomDetectorsActionTypes.LoadManyRoomDetectorsRequest),
-    mergeMap((action) => {
-      return this.roomTemperatureApiService.getAll();
-    }),
-    map((roomDetectors: RoomDetector[]) => {
-      return actions.loadManyRoomDetectorsSuccess({ roomDetectors });
-    }),
-  );
+  // TODO: effect loadManyRoomDetectors$
 
-  @Effect()
-  startSingleRoomTracking$ = this.actions$.pipe(
-    ofType(actions.RoomDetectorsActionTypes.StartSingleRoomTrackingRequest),
-    withLatestFrom(this.isRunning$),
-    filter(([action, isRunning]) => {
-      const { id } = action;
-      return !isRunning[id];
-    }),
-    mergeMap(([action]) => {
-      const { id } = action;
-      return syncRoomTemperatureTask$(
-        this.roomTemperatureApiService,
-        id,
-        this.actions$
-      );
-    }),
-  );
+  // TODO: effect startSingleRoomTracking$
 
   constructor(
     private actions$: Actions,
