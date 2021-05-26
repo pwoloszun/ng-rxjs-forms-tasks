616e8316ddb9efef4a6a2ad1c2740baea2a81b36
Piotr Woloszun <piotr.woloszun@gmail.com>
2021-05-26 10:14:23 +0200

    mysearch p1

--- src/app/features/39-rxjs-gui-tasks/containers/my-search/my-search.component.ts
+++ src/app/features/39-rxjs-gui-tasks/containers/my-search/my-search.component.ts
@@ -1,20 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
 import { FormControl } from '@angular/forms';
-import { of } from 'rxjs';
+import { interval, of, Subscription } from 'rxjs';
 import { debounceTime, distinctUntilChanged, switchMap, mergeMap, exhaustMap } from 'rxjs/operators';
 
-import { SearchApiService } from '../../../../core/api/search-api.service';
+import { SearchApiService } from '@api/search-api.service';
 
 @Component({
   selector: 'nts-my-search',
   templateUrl: './my-search.component.html',
   styleUrls: ['./my-search.component.css']
 })
-export class MySearchComponent {
+export class MySearchComponent implements OnDestroy {
 
   searchText = new FormControl('');
 
+  searchResults$ = this.searchText.valueChanges.pipe(
+
+  );
+
   constructor(private searchApiService: SearchApiService) {
+  }
+
+  // xxx = this.searchApiService.querySearch$('batman!');
+
+  ngOnDestroy() {
   }
 
 }
