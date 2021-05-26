8e8d96c31fdd23c96e1fca378b6a2ec96a79ffc2
Piotr Woloszun <piotr.woloszun@gmail.com>
2021-05-26 07:08:31 +0200

    minors

--- src/app/features/46-many-room-detectors/components/room-thermometer/room-thermometer.component.spec.ts
+++
@@ -1,8 +0,0 @@
-import { RoomThermometerComponent } from './room-thermometer.component';
-
-xdescribe('RoomThermometerComponent', () => {
-
-  it('should create', () => {
-    expect(false).toBeTruthy();
-  });
-});
--- src/app/features/46-many-room-detectors/pages/many-room-detectors/many-room-detectors.component.spec.ts
+++
@@ -1,8 +0,0 @@
-import { ManyRoomDetectorsComponent } from './many-room-detectors.component';
-
-xdescribe('ManyRoomDetectorsComponent', () => {
-
-  it('should create', () => {
-    expect(false).toBeTruthy();
-  });
-});
